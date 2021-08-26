dd472b0d88637b4212a2d28596efc1aad12da5c9
madhur4444 <mk3sharm@edu.uwaterloo.ca>
2021-08-26 22:52:59 +0530

    return balances

--- backend/src/router/index.ts
+++ backend/src/router/index.ts
@@ -20,38 +20,41 @@ export default function router(app: express.Application): void {
     // send addrs as array in req.body
     const addrs = req.body.addrs
     let promises = [];
-
+    let balance_data = [];
+    let balData;
     try {
-        addrs.forEach(async (addr) => {
-        const getAsync = promisify(redis_client.hget).bind(redis_client);
-        const hkey = Buffer.from(`${addr}`).toString("base64");
-        const balData = await getAsync(hkey, addr);
-        if(balData){
-          console.log(balData)
-        } else{
-          const prom = axios.get(`https://safe-transaction.gnosis.io/api/v1/safes/${addr}/balances/usd/`)
-            .then((res) => {
-              // set expiry to an hour
-              redis_client.hset(hkey,addr, JSON.stringify(res.data))
-              redis_client.expire(hkey, 60*60)
-            })
-            // Put all reqs together so we can run concurrently 
-          promises.push(prom);
+        for(let addr of addrs) {
+          const getAsync = promisify(redis_client.hget).bind(redis_client);
+          const hkey = Buffer.from(`${addr}`).toString("base64");
+          balData = await getAsync(hkey, addr);
+          if(balData){
+            // console.log(balData)
+            balance_data.push(balData);
+          } else{
+            // console.log("adding to cache")
+            const prom = axios.get(`https://safe-transaction.gnosis.io/api/v1/safes/${addr}/balances/usd/`)
+              .then((res) => {
+                // set expiry to an hour
+                redis_client.hset(hkey,addr, JSON.stringify(res.data))
+                redis_client.expire(hkey, 60*60)
+                balance_data.push(JSON.stringify(res.data))
+              })
+          // Put all reqs together so we can run concurrently 
+            promises.push(prom);
+          }
         }
-      });
       
       // Resolve all promises if added
-      Promise.all(promises)
-      .then( () => {
-      })
-      .catch(console.error);
-
+      await Promise.all(promises).catch(err =>{
+        console.log(err)
+      });
+      
+      // console.log(balance_data)
       // Return success if everything happens smoothly
       res
       .status(200)
-      .send({
-        message: "Successfully Queried"
-      })
+      .json(balance_data)
+      
     } catch (error) {
      console.log(error);
   }
